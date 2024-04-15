2d4ce941a9fde58f7d15e3926292bf182050e87a
Márton László Szabó <marton.szabo2@stud.uni-corvinus.hu>
2024-04-15 15:07:24 +0200

    jog

--- Legyenmarjo/Legyenmarjo/Form1.cs
+++ Legyenmarjo/Legyenmarjo/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Legyenmarjo.Entities;
+using Legyenmarjo.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +12,46 @@ using System.Windows.Forms;
 
 namespace Legyenmarjo
 {
+
+
+
     public partial class Form1 : Form
     {
+
+        BindingList<RateData> Rates = new BindingList<RateData>();  
+
+
         public Form1()
         {
             InitializeComponent();
+            // A változó deklarációk jobb oldalán a "var" egy dinamikus változó típus.
+            // A "var" változó az első értékadás pillanatában a kapott érték típusát veszi fel, és később nem változtatható.
+            // Jelen példa első sora tehát ekvivalens azzal, ha a "var" helyélre a MNBArfolyamServiceSoapClient-t írjuk.
+            // Ebben a formában azonban olvashatóbb a kód, és változtatás esetén elég egy helyen átírni az osztály típusát.
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
+            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
+            var response = mnbService.GetExchangeRates(request);
+
+            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
+            // Ezért a result változó valójában string típusú.
+            var result = response.GetExchangeRatesResult;
+
+            Console.WriteLine(result);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Rates.ToList();
         }
     }
 }
